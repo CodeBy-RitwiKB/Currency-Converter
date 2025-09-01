73b1af7dca544a8f9f00387ef3146f82cce9db1f
Ritwik Banerjee <kritwikbanerjee27@gmail.com>
2025-09-01 21:40:38 +0530

    Update the feature

---
+++ script.js
@@ -0,0 +1,56 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const exchangeRateTxt = document.querySelector(".exchange-rate");
+const amountInput = document.querySelector(".amount-input");
+
+for(let select of dropdowns){
+    for (currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD"){
+            newOption.selected ="selected";
+        } else if(select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+});
+}
+
+
+
+const updateFlag =(element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    element.parentElement.querySelector("img").src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amtVal = amountInput.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = "1";
+        amountInput.value = "1";
+    }
+
+    
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    
+    let finalAmount = (rate * amtVal);
+
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    
+});
